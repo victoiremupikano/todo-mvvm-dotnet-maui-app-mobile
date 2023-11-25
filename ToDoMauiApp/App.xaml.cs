dce8dcc94282b45c521ea9e02e0983dc794beaea
victoirempkn <96738243+victoiremupikano@users.noreply.github.com>
2023-11-25 20:23:55 +0200

    Add project files.

---
+++ ToDoMauiApp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ToDoMauiApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ToDoMauiApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoMauiApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
